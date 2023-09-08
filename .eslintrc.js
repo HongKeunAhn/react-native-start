3158559bd5b7c8ab109da561c9eae6146ccb339d
Alda_kyle <kyle@Alda-kyleui-MacBookPro.local>
2023-09-08 09:23:46 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};

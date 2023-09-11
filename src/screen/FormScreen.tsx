303e8e99ff99e02d6011ab3887ac5eaf50fd788e
Keu.ni <skcor8909@gmail.com>
2023-09-11 11:25:07 +0900

    <feat> input Value storage

--- src/screen/FormScreen.tsx
+++ src/screen/FormScreen.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  TextInput,
+  StyleSheet,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const FormScreen = () => {
-  return <TextInput style={styles.input} />;
+  const [input, setInput] = useState<string>('');
+
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('my-text', value);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const handleTextInputChange = (
+    event: NativeSyntheticEvent<TextInputChangeEventData>,
+  ) => {
+    const { text } = event.nativeEvent;
+
+    storeData(text);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('my-text');
+        if (value !== null) {
+          // value previously stored
+          setInput(value);
+        }
+      } catch (e) {
+        // error reading value
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <TextInput
+      style={styles.input}
+      value={input}
+      onChange={handleTextInputChange}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
